bdbbb19ce137035002f49c140259c52df2b5efae
dongbox <sfreebobo@163.com>
2021-03-16 17:56:12 +0800

    Fix a expression error in contant.

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -51,7 +51,7 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I'm currently preparing the TOEFL test to kepp going my master and not looking for
+        Although I'm currently preparing the TOEFL test for applying the master and not looking for
         any new opportunities, my inbox is always open. Whether you have a question or just want to
         say hi, I'll try my best to get back to you!
       </p>
